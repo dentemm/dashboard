78192758c0e6d4cf235ce28b418983d2734586e5
Tim Claes <tim.claes@me.com>
2016-06-11 14:24:03 +0200

    grr

--- dashboard/wsgi.py
+++ dashboard/wsgi.py
@@ -14,7 +14,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings.dev")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dashboard.settings.production')
 
 application = get_wsgi_application()
