f054d7da52caae6457e83022660b7cc88745317a
Tim Claes <tim.claes@me.com>
2016-06-11 12:34:50 +0200

    yet another change

--- dashboard/settings/production.py
+++ dashboard/settings/production.py
@@ -14,18 +14,6 @@ except ImportError:
 
 # ZIE WEB HOSTING DOCUMENTATIE VOOR MEER INFO OVER ONDERSTAANDE SETTINGS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dashboard_db',                      # Or path to database file if using sqlite3.
-        #'NAME': 'test',
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-    }
-} 
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
