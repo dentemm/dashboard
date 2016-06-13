45b33dad998e4e3dbb76dad5f4c691b349e40176
Tim Claes <tim.claes@me.com>
2016-06-13 12:50:51 +0200

    Some additions

--- home/models.py
+++ home/models.py
@@ -1,9 +1,74 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.db import models
+from django.db import models as djangomodels
+from django.conf import settings
 
-from wagtail.wagtailcore.models import Page
+from wagtail.wagtailcore import models
+from wagtail.wagtailadmin.edit_handlers import StreamFieldPanel, FieldPanel, InlinePanel, PageChooserPanel, MultiFieldPanel, FieldRowPanel
+from wagtail.wagtailsnippets.models import register_snippet
+
+from modelcluster.fields import ParentalKey
+
+@register_snippet
+class Task(djangomodels.Model):
+
+	title = djangomodels.CharField(max_length=63)
+	description = djangomodels.CharField(max_length=510)
+	owner = djangomodels.ForeignKey(settings.AUTH_USER_MODEL)
+
+	page = ParentalKey('home.SafeWorkingPermit', related_name='tasks')
+
+Task.panels = [
+	
+	FieldPanel('title'),
+	FieldPanel('description')
+]
+
+class SafeWorkingPermit(models.Page):
+
+	vwv_owner = djangomodels.ForeignKey(settings.AUTH_USER_MODEL)
+	vwv_title = djangomodels.CharField(max_length=63)
+	description = djangomodels.CharField(max_length=510)
+
+# Panel definitions for SafeWorkingPermit page
+SafeWorkingPermit.content_panels = models.Page.content_panels + [
+
+	FieldPanel('title'),
+	InlinePanel('tasks', label='Tasks')
+]
 
 
-class HomePage(Page):
+class SafeworkingPermitUser(djangomodels.Model):
+
+	user = djangomodels.ForeignKey(settings.AUTH_USER_MODEL)
+	page = ParentalKey('home.SafeWorkingPermit', related_name='involved_parties')
+	role = djangomodels.CharField(max_length=8) # Welke rol heeft deze gebruiker voor een gegeven Veilig Werk Vergunning?
+
+	
+
+
+
+class ToolPage(models.Page):
+
+	name = djangomodels.CharField(max_length=63)
+
+# Panel definitions for ToolPage
+ToolPage.content_panels = models.Page.content_panels + [
+
+	FieldPanel('name'),
+	InlinePanel('modules', label='Modules')
+]
+
+@register_snippet
+class Module(djangomodels.Model):
+
+	page = ParentalKey('home.ToolPage', related_name='modules', null=True, blank=True)
+	name = djangomodels.CharField(max_length=50)
+
+Module.panels = [
+	
+	FieldPanel('name'),
+]
+
+class HomePage(models.Page):
     pass
--- manage.py
+++ manage.py
@@ -5,8 +5,9 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings.production")
+	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings.dev")
+	#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings.production")
 
-    from django.core.management import execute_from_command_line
+	from django.core.management import execute_from_command_line
 
-    execute_from_command_line(sys.argv)
+	execute_from_command_line(sys.argv)
