d5a69c0a67b6d65f28603f6b14a434aa57fd0104
Tim Claes <tim.claes@me.com>
2016-08-04 17:07:32 +0200

    .env file added

--- dashboard/settings/production.py
+++ dashboard/settings/production.py
@@ -1,11 +1,14 @@
 from __future__ import absolute_import, unicode_literals
 
+import os
+
+env = os.environ.copy()
+SECRET_KEY = env['SECRET_KEY']
+
 from .base import *
 
 DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9oy(88fy6$p6m!2jx^xf&j5)gw2xim(s%qvz*s+7ps^-equlst'
 
 try:
     from .local import *
