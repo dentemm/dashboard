9fdba7aebdb13f9bbe9024fc0f046ae1ba18921e
Tim Claes <tim.claes@me.com>
2016-08-13 19:22:15 +0200

    temporary removed compress dependancy

--- dashboard/settings/base.py
+++ dashboard/settings/base.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Third party
-    'compressor',
+    #'compressor',
 
     # custom apps
     #'utilities',
@@ -132,7 +132,7 @@ USE_TZ = True
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
+    #'compressor.finders.CompressorFinder',
 ]
 
 STATICFILES_DIRS = [
